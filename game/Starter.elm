9b9b5e08f91fa2a2fbf0893dfccd800efffad318
Willem van Heemstra <wvanheemstra@icloud.com>
2015-09-15 18:03:22 +0100

    Added: initial files for creating an app that behaves like a game

---
+++ game/Starter.elm
@@ -0,0 +1,47 @@
+module Starter where
+
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+
+
+-- MODEL
+
+type alias Model = Float
+
+
+-- UPDATE
+
+type Action = Increment | Decrement | Double | Half
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Increment -> model + 1
+    Decrement -> model - 1
+    Double -> model * 2
+    Half -> model / 2
+
+
+-- VIEW
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div []
+    [ button [ onClick address Half ] [ text "/2" ]
+    , button [ onClick address Decrement ] [ text "-" ]
+    , div [ countStyle ] [ text (toString model) ]
+    , button [ onClick address Increment ] [ text "+" ]
+    , button [ onClick address Double ] [ text "*2" ]
+    ]
+
+
+countStyle : Attribute
+countStyle =
+  style
+    [ ("font-size", "20px")
+    , ("font-family", "monospace")
+    , ("display", "inline-block")
+    , ("width", "50px")
+    , ("text-align", "center")
+    ]
